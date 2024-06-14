8cf7c7023db7108b62df13b0050b77b73ca1fd96
danil <vinniopo@mail.ru>
2024-06-14 10:50:34 +0700

    added initial static files - basetemplate, bootstrap css and js

--- job/urls.py
+++ job/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 app_name = 'job'
 
 urlpatterns = [
- #   path('', views.main, name='main')
-]
+   path('', views.main, name='main')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
