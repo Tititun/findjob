4e0deaad3b06b83dbaadd29bc0bd2b823e63afce
danil <vinniopo@mail.ru>
2024-06-24 15:07:31 +0700

    added education and experience page

--- job/urls.py
+++ job/urls.py
@@ -5,5 +5,6 @@ app_name = 'job'
 
 urlpatterns = [
    path('projects/<str:project_id>', views.projects, name='projects'),
+   path('education', views.education, name='education'),
    path('', views.main, name='main'),
 ]
--- job/views.py
+++ job/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 
 def main(request):
-    print(request.headers)
     return render(request, 'job/main.html')
 
 
 def projects(request, project_id):
     return render(request, f'job/projects/{project_id}.html')
+
+
+def education(request):
+    return render(request, 'job/education.html')
